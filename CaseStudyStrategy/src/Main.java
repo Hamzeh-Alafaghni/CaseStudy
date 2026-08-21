f275281810a0b81eea3b2d103b6c97c0071d6e5b
Hamzeh-Alafaghni <hamzehw32@gmail.com>
2026-08-21 21:27:03 +0300

    add example in main + add my AI prompt

--- CaseStudyStrategy/src/Main.java
+++ CaseStudyStrategy/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     static void main(String[] args) {
 
+        ClinicPatient patient = new ClinicPatient(new CashPayment());
+        patient.payBill(50);
 
+        System.out.println("Patient switched payment method...");
+        patient.setPaymentStrategy(new CardPayment());
+        patient.payBill(50);
     }
 }
